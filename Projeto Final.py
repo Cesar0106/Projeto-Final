26d970882e4ee8e0bb631172641cc935ddc0684e
jonathansutton1 <62657975+jonathansutton1@users.noreply.github.com>
2020-05-28 16:08:37 -0300

    ajuste de imagens

--- Projeto Final.py
+++ Projeto Final.py
@@ -11,10 +11,13 @@ clock=pygame.time.Clock()
 
 x = 10 #posicao do x em pixel
 y = 330 #posicao do y em pixel 
-y_inimigos = 330
+y_robo = 330
+y_asteroide = 480
+y_nave = 230
 x_robo = 800
 x_asteroide = 800
 x_nave = 800
+
 
 velocity = 10 #velocidade em pixels
 velocity_enemies = 15
@@ -38,7 +41,7 @@ BLACK = (0,0,0)
 
 
 window = pygame.display.set_mode((700, 700)) # tamanho da janela
-pygame.display.set_caption('Joguinho das bolinhas') #nome que aparece na janela
+pygame.display.set_caption('Invasores de Petrópolis') #nome que aparece na janela
 controle  = 0
 game = True
 while True: # cria o jogo
@@ -48,22 +51,37 @@ while True: # cria o jogo
             game = False #desliga o jogo
             sys.exit()
 
-    controle+=1
-    print (controle)
+        
 
-    comandos = pygame.key.get_pressed()
-    if comandos[pygame.K_UP] and y >=220: 
+    controle+=1
+    
+
+    movimentos = pygame.key.get_pressed()
+    if movimentos[pygame.K_UP] and y >=220: 
         y -= velocity # subir
-    if comandos[pygame.K_LEFT] and x >= 0:
+    if movimentos[pygame.K_LEFT] and x >= 0:
         x -= velocity # ir pra esquerda
-    if comandos[pygame.K_RIGHT] and x <= 600:
+    if movimentos[pygame.K_RIGHT] and x <= 600:
         x += velocity #ir pra direita
-    if comandos[pygame.K_DOWN] and y <=520:
+    if movimentos[pygame.K_DOWN] and y <=520:
         y += velocity # ir pra baixo
 
-    if x_robo <= -100 and x_asteroide <= -100 and x_nave <= -100:
+    if x + 30 > x_robo and x - 30 < x_robo and y - 30 > y_robo and y + 20 < y_robo:
+        x = 900
+    #if x + 10 > x_asteroide and x - 10 < x_asteroide and y > y_asteroide:    
+        #x = 2000       
+    #if x + 10 > x_nave and y - 30 < y_nave:   
+        #x = 2000
+        
+        
+
+    if x_robo <= -100:   
         x_robo = random.randint(800,2000) #stormtrooper chegando da direita
+
+    if x_asteroide <= -100:
         x_asteroide = random.randint(800,2000) #asteroide chegando da direita
+
+    if x_nave <= -100:
         x_nave = random.randint(800,4000) #nave chegando da direita
 
     x_robo -= velocity_enemies + random.randint(1,10) #velocidade aleatoria
@@ -73,12 +91,9 @@ while True: # cria o jogo
     window.fill ((BLUE))#Fundo azul
     window.blit(fundo,(0,0))
     window.blit(carinha,(x,y))      
-    window.blit(trooper,(x_robo,y_inimigos))
-    window.blit(asteroide,(x_asteroide,y_inimigos + 150))
-    window.blit(nave,(x_nave,y_inimigos - 100))
-
-    
-
+    window.blit(trooper,(x_robo,y_robo))
+    window.blit(asteroide,(x_asteroide,y_asteroide))
+    window.blit(nave,(x_nave,y_nave))
 
     pygame.draw.line(window,BLACK,[0,600],[800,600],5)   #linha 1
     pygame.draw.line(window,BLACK,[0,200],[800,200],5)   #linha 2
